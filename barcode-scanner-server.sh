9ca1f915661b4d4aae2653f4d11967e45ef40063
Martin Lonnemann <melonnemann@gmail.com>
2024-07-18 12:45:05 -0400

    edit

--- barcode-scanner-server.sh
+++ barcode-scanner-server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cd "`dirname $(readlink -f ${0})`"
+
+echo $(pwd )
 source ./venv/bin/activate
 python3 barcode-scanner-server.py
--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd "`dirname $(readlink -f ${0})`"
-python -m venv ./venv
+sudo ln -sf $(pwd)/barcode-scanner-server.sh /usr/local/bin/barcode-scanner-server
+python3 -m venv ./venv
 source ./venv/bin/activate
 pip install -r requirements.txt
